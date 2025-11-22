55bdef141a1442f1a8e8432da0708367eddc7c08
Natan Teixeira <natann875@gmail.com>
2025-11-21 23:13:55 -0300

    Atrelçao endereco com usuario

--- backend/public_html/db/migrations/20251026173135_create_table_categoria.php
+++ backend/public_html/db/migrations/20251026173135_create_table_categoria.php
@@ -13,13 +13,16 @@ final class CreateTableCategoria extends AbstractMigration
         if ($this->hasTable($tableName)) {
             $this->table($tableName)->drop()->save();
         }
-        
-        $table = $this->table($tableName,['id'=>'categoria_id']);
-        $table->addColumn('nome','string',['limit'=> 255,'null'=> false])
-        ->addColumn('usuario_id','integer',['limit'=> 11,'signed'=> false,'null'=> true])
-        ->addColumn('sis_cat','boolean',['default'=>0])
-        ->addTimestamps('data_criacao', 'data_atualizacao')
-        ->create();
 
+        $table = $this->table($tableName, ['id' => 'categoria_id']);
+        $table->addColumn('nome', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('usuario_id', 'integer', ['limit' => 11, 'signed' => false, 'null' => true])
+            ->addColumn('sis_cat', 'boolean', ['default' => 0])
+            ->addTimestamps('data_criacao', 'data_atualizacao')
+            ->addForeignKey('usuario_id', 'usuario', 'usuario_id', [
+                'delete' => 'CASCADE',
+                'update' => 'NO_ACTION'
+            ])
+            ->create();
     }
 }
