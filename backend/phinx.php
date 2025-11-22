ed7a91051ca08d4d3585e4231d0e33baa166000c
Natan Teixeira <natann875@gmail.com>
2025-11-22 18:15:19 -0300

    Alteraçao em phinx

--- backend/phinx.php
+++ backend/phinx.php
@@ -1,41 +1,31 @@
 <?php
 
-return
-    [
-        'paths' => [
-            'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-            'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+return [
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => getenv('MYSQL_HOST'),
+            'name' => getenv('MYSQL_DATABASE'),
+            'user' => getenv('MYSQL_USER'),
+            'pass' => getenv('MYSQL_PASSWORD'),
+            'port' => getenv('MYSQL_PORT'),
+            'charset' => 'utf8mb4'
         ],
-        'environments' => [
-            'default_migration_table' => 'phinxlog',
-            'default_environment' => 'development',
-            'production' => [
-                'adapter' => 'mysql',
-                'host' => 'localhost',
-                'name' => 'production_db',
-                'user' => 'root',
-                'pass' => '',
-                'port' => '3306',
-                'charset' => 'utf8',
-            ],
-            'development' => [
-                'adapter' => 'mysql',
-                'host' => 'mysql',       // nome do serviço Docker
-                'name' => 'medhub',
-                'user' => 'developmentMedHub',
-                'pass' => 'dev123',
-                'port' => '3306',        // porta interna do MySQL no container
-                'charset' => 'utf8mb4'
-            ],
-            'testing' => [
-                'adapter' => 'mysql',
-                'host' => 'localhost',
-                'name' => 'testing_db',
-                'user' => 'root',
-                'pass' => '',
-                'port' => '3306',
-                'charset' => 'utf8',
-            ]
-        ],
-        'version_order' => 'creation'
-    ];
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => getenv('MYSQL_HOST'),
+            'name' => getenv('MYSQL_DATABASE'),
+            'user' => getenv('MYSQL_USER'),
+            'pass' => getenv('MYSQL_PASSWORD'),
+            'port' => getenv('MYSQL_PORT'),
+            'charset' => 'utf8'
+        ]
+    ],
+    'version_order' => 'creation'
+];
