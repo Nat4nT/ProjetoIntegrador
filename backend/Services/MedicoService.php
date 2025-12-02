439b48817a11b467f655c8d7a7b08553bd674805
Natan Teixeira <natann875@gmail.com>
2025-12-01 21:52:05 -0300

    Id autorizacao acesso

--- backend/Services/MedicoService.php
+++ backend/Services/MedicoService.php
@@ -116,6 +116,7 @@ class MedicoService
         if ($pacientes) {
             foreach ($pacientes as $paciente) {
                 $dados[] = [
+                    'autorizacao_acesso_id' => $paciente['autorizacao_acesso_id'],
                     'status' => $paciente['status'],
                     'data_atualizacao' => $paciente['data_atualizacao'],
                     'paciente_id' => $paciente['paciente_id'],
